765b3f9c804c2f7a8c60fc0c0485cfcd23aeda09
Shortyss <Shortyss@seznam.cz>
2023-11-30 18:00:54 +0100

    add str to models

--- viewer/models.py
+++ viewer/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.db.models import Model, CharField, IntegerField, TextField, DateField, ForeignKey, DO_NOTHING, \
-    ManyToManyField, SET_NULL
+    ManyToManyField, SET_NULL, DateTimeField
 from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -26,8 +27,11 @@ class Genre(Model):
 class Person(Model):
     first_name = CharField(max_length=32, null=False, blank=False)
     last_name = CharField(max_length=32, null=False, blank=False)
+    nationality = CharField(max_length=64, null=True, blank=True)
     birth_date = DateField(null=True, blank=True)
     biography = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -46,7 +50,9 @@ class Movie(Model):
     actors = ManyToManyField(Person, blank=True, related_name='acting_in_movie')
     year = IntegerField(null=True, blank=True)
     video = CharField(max_length=128, null=True, blank=True)
-    description = TextField(null=True, blank=True)   # TextField je dobrý pro dlouhé psaní
+    description = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
 
     def __str__(self):
         if self.title_cz:
@@ -57,9 +63,10 @@ class Movie(Model):
             title_name = self.title_orig
 
         if self.year is not None:
-            title_name += f"({self.year})"
+            title_name += f" ({self.year})"
 
         return title_name
+
 
 class Rating(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
@@ -67,16 +74,18 @@ class Rating(Model):
     rating = IntegerField(null=False, blank=False)
 
     def __str__(self):
-        return f"{self.user}: {self.rating}"
+        return f"{self.movie}: {self.rating} by {self.user}"
 
 
 class Comment(Model):
     movie = ForeignKey(Movie, on_delete=DO_NOTHING, null=False, blank=False)
     user = ForeignKey(User, null=True, on_delete=SET_NULL)
     comment = TextField(null=False, blank=False)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.user}: {self.comment}"
+        return f"{self.movie}: {self.user} {self.comment[:20]}"
 
 
 class Image(Model):
